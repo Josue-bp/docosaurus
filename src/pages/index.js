2292ef827b4f23a1189c6fba6dc44c5b11aae0b0
Josue-bp <josueborja1231@gmail.com>
2025-04-23 18:30:37 -0500

    Al dia con gh-pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,30 +15,40 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+        <Link to="/docs/intro">
         <img
     src="https://i.pinimg.com/originals/de/30/21/de3021c5618a58faa9f39ba3df133f24.gif"
     alt="computadora"
     className={styles.robotGif}
   />
+  </Link>
 </div>
 
         {/* Iconos tipo gamer/dev con URL */}
         <div className={styles.imagesWrapper}>
+        <Link to="/docs/tutorial-basics/elementos">
           <img
             className={styles.heroImage}
             src="https://img.icons8.com/fluency/96/source-code.png"
             alt="Code Icon"
           />
+          </Link>
+          <Link to="/docs/tutorial-basics/consola">
+
           <img
             className={styles.heroImage}
             src="https://img.icons8.com/color/96/console.png"
             alt="Console Icon"
           />
+          </Link>
+          <Link to="/docs/tutorial-basics/fuentes">
+
           <img
             className={styles.heroImage}
             src="https://img.icons8.com/fluency/96/laptop.png" 
             alt="Laptop Icon"
           />
+          </Link>
         </div>
       </div>
     </header>
@@ -56,17 +66,27 @@ export default function Home() {
   <section className={styles.featureSection}>
     <div className={styles.featureContainer}>
       <div className={styles.featureItem}>
+      <Link to="/docs/tutorial-basics/lighthouse">
       <img src="https://img.icons8.com/fluency/96/idea.png" alt="Idea icon" />
+      </Link>
+
       <h3>Fácil de Usar</h3>
         <p>Diseñado para desarrolladores principiantes y gamers con estilo pro.</p>
       </div>
       <div className={styles.featureItem}>
+      <Link to="/docs/tutorial-basics/aplicacion">
+
         <img src="https://img.icons8.com/fluency/96/source-code.png" alt="Code icon" />
+        </Link>
+
         <h3>Código Limpio</h3>
         <p>Estructura clara, mantenible y adaptable a cualquier proyecto.</p>
       </div>
       <div className={styles.featureItem}>
+      <Link to="/docs/tutorial-basics/red">
         <img src="https://img.icons8.com/fluency/96/cloud.png" alt="Cloud icon" />
+        </Link>
+
         <h3>Deploy Sencillo</h3>
         <p>Compatible con GitHub Pages, Vercel y Netlify en pocos clics.</p>
       </div>
