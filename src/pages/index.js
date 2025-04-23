ffa30338cf19c00b736f330683e7da7fbb6b6bd7
Josue-bp <josueborja1231@gmail.com>
2025-04-22 20:18:55 -0500

    componente: agregar encabezado personalizado con iconos y enlaces a la página de tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -17,11 +15,28 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            Ver Tutorial - 5min ⏱️
           </Link>
+        </div>
+
+        {/* Iconos tipo gamer/dev con URL */}
+        <div className={styles.imagesWrapper}>
+          <img
+            className={styles.heroImage}
+            src="https://img.icons8.com/fluency/96/source-code.png"
+            alt="Code Icon"
+          />
+          <img
+            className={styles.heroImage}
+            src="https://img.icons8.com/color/96/console.png"
+            alt="Console Icon"
+          />
+          <img
+            className={styles.heroImage}
+            src="https://img.icons8.com/fluency/96/laptop.png" 
+            alt="Laptop Icon"
+          />
         </div>
       </div>
     </header>
@@ -32,11 +47,11 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Bienvenido a ${siteConfig.title}`}
+      description="Plataforma para desarrolladores principiantes con estilo gamer/pro.">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* Puedes dejar esto o poner otros componentes */}
       </main>
     </Layout>
   );
