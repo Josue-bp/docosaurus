02d27c19f5e3e5a4d657ebab6e86811b21c4ccb6
Josue-bp <josueborja1231@gmail.com>
2025-04-23 19:53:57 -0500

    chore: cierre de configuración y estructura

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://Josue-bp.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docosaurus',
+  baseUrl: '/docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Josue-bp', // Usually your GitHub org/user name.
-  projectName: 'docosaurus', // Usually your repo name.
+  projectName: 'docusaurus', // Usually your repo name.
   deploymentBranch:'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
